c31b73148bebd39be671bb461ad9760573bfe0c8
DeBraid <de.braid@gmail.com>
2015-03-27 19:12:46 -0400

    data with args

--- client/main.js
+++ client/main.js
@@ -1,27 +1,21 @@
-Meteor.call('getScrapedTableData', function (error, result) {
-  if (error) {
-    console.log("error", error);
-  };
+ARGS = {
+    pageNum: '1',
+    org: 'electricity',
+    year: '2014'
+}
 
-  console.log(result);
-  return Session.set("tableData", result);
+Meteor.call('getScrapedTableData', ARGS, function (error, result) {
+    if (error) {
+        console.log("error", error);
+    };
+
+    console.log(result);
+    return Session.set("tableData", result);
 
 });
 
 Template.salaryTable.helpers({
-  rant: function () {
-    return Session.get("tableData");
-  }
+    rant: function () {
+        return Session.get("tableData");
+    }
 });
-
-// Fetch the data from the table body
-// var tableObject = $("#tableID tbody tr.tableClass").map(function(i){  
-//   var row = {};
-//     $(this).find('td').each(function(i){
-//       var rowName = headers[i];
-//       row[rowName] = $(this).text();
-//     }); 
-//   return row;
-//  }).get();
-// // convert object to JSON
-// JSON.stringify(tableObject);
--- server/server.js
+++ server/server.js
@@ -1,32 +1,32 @@
 Meteor.startup(function () {
-var cheerio = Meteor.npmRequire('cheerio');
+    var cheerio = Meteor.npmRequire('cheerio');
 
-Meteor.methods({
-  getScrapedTableData: function (pageNum, org, year) {
-    var rootUrl = "http://www.fin.gov.on.ca/en/publications/salarydisclosure/pssd/orgs-tbs.php",
-        pageNum = "?pageNum_tbs=0",
-        org = "&organization=electricity",
-        year = "&year=2014";
-    result = Meteor.http.get( rootUrl + pageNum + org + year );
-    $ = cheerio.load(result.content);
+    Meteor.methods({
+        getScrapedTableData: function (ARGS) {
+            var rootUrl = "http://www.fin.gov.on.ca/en/publications/salarydisclosure/pssd/orgs-tbs.php",
+                pageNum = "?pageNum_tbs=" + ARGS.pageNum,
+                org = "&organization=" + ARGS.org,
+                year = "&year=" + ARGS.year;
+            result = Meteor.http.get(rootUrl + pageNum + org + year);
+            $ = cheerio.load(result.content);
 
-    var headers = $('#right_column > div:nth-child(2) > table > thead > tr:nth-child(3) th strong')
-        .clone()    //clone the element
-        .children() //select all the children
-        .remove()   //remove all the children
-        .end()  //again go back to selected element
-        .text()
-        .split(" / ");             
+            var headers = $('#right_column > div:nth-child(2) > table > thead > tr:nth-child(3) th strong')
+                .clone() //clone the element
+                .children() //select all the children
+                .remove() //remove all the children
+                .end() //again go back to selected element
+                .text()
+                .split(" / ");
 
-    return $('#right_column > div:nth-child(2) > table > tbody > tr')
-        .map(function(i){  
-            var row = {};
-            $(this).find('td').each(function(i){
-              var rowName = headers[i];
-              row[rowName] = $(this).text();
-            }); 
-          return row;
-         }).get();
-  }
-})
+            return $('#right_column > div:nth-child(2) > table > tbody > tr')
+                .map(function (i) {
+                    var row = {};
+                    $(this).find('td').each(function (i) {
+                        var rowName = headers[i];
+                        row[rowName] = $(this).text();
+                    });
+                    return row;
+                }).get();
+        }
+    })
 });
