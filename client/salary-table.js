a0eb8816d67ca2af3076ef3c2401aaf8db4708f4
DeBraid <de.braid@gmail.com>
2015-03-28 14:46:26 -0400

    added kadira, testing git ignore settings.json

--- client/salary-table.js
+++ client/salary-table.js
@@ -1,3 +1,13 @@
+Template.salaryTable.helpers({
+  settings: function () {
+    return {
+      collection: Schools.find({}).fetch(),
+      rowsPerPage: 20,
+      showFilter: true
+    };
+  }
+});
+
 // Template.salaryTable.helpers({
 //   // top100schools: function () {
 //   //   return Schools.find({ 'Employer' : 'Hamilton-Wentworth District School Board'}).fetch();
@@ -12,12 +22,27 @@
 //   // }
 // });
 
-Template.salaryTable.helpers({
-  settings: function () {
-    return {
-      collection: Schools.find({}).fetch(),
-      rowsPerPage: 20,
-      showFilter: true
-    };
-  }
-});
+// ARGS = {
+//     pageNum: '6',
+//     org: 'municipalities',
+//     year: '2014'
+// }
+
+// Meteor.call('getScrapedTableData', ARGS, function (error, result) {
+//     if (error) {
+//         console.log("error", error);
+//     };
+
+//     console.log(result);
+//     // Salaries.insert(result, function () {
+//     //     console.log("GO GO GADGET salaries created");
+//     // });
+//     return Session.set("tableData", result);
+
+// });
+
+// Template.salaryTable.helpers({
+//     // rant: function () {
+//     //     return Session.get("tableData");
+//     // }
+// });
